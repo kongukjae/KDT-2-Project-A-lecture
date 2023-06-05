02f6d2a5d996494a3b7acb5bada68b97ac9769db
kongukjae <kongukjae@gmail.com>
2023-06-05 17:43:16 +0900

    front-end, architecting 기본 예시

--- build/src/App.tsx
+++ build/src/App.tsx
@@ -1,9 +1,13 @@
 import "./test.css"
 import React from 'react';
+import Test from './view/components/common/test';
+import Header from './view/components/layout/header';
 
 const App = () => {
     return (
         <div>
+            <Header />
+            <Test />
             <h1>React App</h1>
         </div>
     );
