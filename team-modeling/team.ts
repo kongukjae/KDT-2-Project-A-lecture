28373a04de8b7b5d3e6688fa637c995309ac6b54
kongukjae <kongukjae@gmail.com>
2023-06-20 10:53:08 +0900

    팀 결정 로직 만들기 구상

---
+++ team-modeling/team.ts
@@ -0,0 +1,24 @@
+const demoStudent : Object  = {
+  studentName : "kongukjae",
+  teamProjectC : [],
+  teamProjectA : [],
+  teamProjectB : [],
+}
+
+/**
+ * work : 1. 24명의 학생마다의 데이터를 생성한다.
+ * work : 2. 결정 되었던 팀 객체 생성
+ * work : 2-1. 1개의 팀은 구성원이 나타나야 한다.
+ */
+
+const demoTeam : Object = {
+  teamName : "KDT",
+  members : [],
+  teamNumber : 0,
+}
+
+function demoTeamDecision( totalStudent:string[] ):object {
+  let value : object = {};
+
+  return value;
+}
