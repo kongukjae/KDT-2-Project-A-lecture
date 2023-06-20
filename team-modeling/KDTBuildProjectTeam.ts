a7a141346a671670303ec60d70e79fdfc3651cc1
kongukjae <kongukjae@gmail.com>
2023-06-20 13:00:29 +0900

    팀 하나에 대한 생성자 함수, 설명 주석 처리 모듈작업 완료

--- team-modeling/KDTBuildProjectTeam.ts
+++ team-modeling/KDTBuildProjectTeam.ts
@@ -13,6 +13,17 @@ interface interfaceKDTBuildProjectTeam {
   teamMembers: string[];
 }
 
+/**
+ * 클래스 KDTBuildProjectTeam 설명
+ * @constructor
+ * @param {number} teamNumber - 팀 번호
+ * @param {string} teamName - 팀 이름
+ * @param {string} teamProjectManagerName - 팀장 이름
+ * @param {string[]} teamMembers - 팀원 이름
+ * @returns {object} - 팀 정보를 담고 있는 객체
+ * @example
+ * const team = new KDTBuildProjectTeam(1, "KDT", "홍길동", ["춘향이", "그린컴"]);
+ */
 class KDTBuildProjectTeam implements interfaceKDTBuildProjectTeam {
   teamNumber: number;
   teamName: string;
