72cf06d3a312dc85ed005db024cabcec93125071
kongukjae <kongukjae@gmail.com>
2023-06-20 13:01:04 +0900

    프로젝트 목록을 생성하는 생성자함수, 주석 처리 모듈작업 완료

--- team-modeling/KDTProjectList.ts
+++ team-modeling/KDTProjectList.ts
@@ -1,16 +1,30 @@
-import KDTBuildProjectTeam from "./KDTBuildProjectTeam";
+/**
+ * 클래스 KDTBuildProjectList 설명
+ * @constructor
+ * @param {string[]} projectName - 프로젝트 이름
+ * @returns {object} - 프로젝트 이름을 키로 하는 객체
+ * @example 
+ * const projectList = new KDTBuildProjectList("teamProjectA", "teamProjectB" ,"teamProjectC");
+ * 
+ * @ get getProjectList - 프로젝트 이름을 배열로 반환
+ * 접근자(accessor) 이므로 함수가 아님
+ * 함수호출 방식이 아닌 변수처럼 사용
+ * @example
+ * projectList.getProjectList;
+ *
+ */
 
-// Todo : Project 목록을 확인할 수 있는 클래스여야 한다.
-// work : getter로 처리함
-class KDTProjectList {
+class KDTBuildProjectList {
   [key: string]: object;
+
   constructor(...projectName: string[]) {
     for (const name of projectName) {
       this[name] = {};
     }
   }
-
   get getProjectList(): string[] {
     return Object.keys(this);
   }
 }
+
+export default KDTBuildProjectList;
