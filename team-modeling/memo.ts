278dfb0d02dc9f604c8b37a3a1944f1fe7f8497f
kongukjae <kongukjae@gmail.com>
2023-06-20 11:37:02 +0900

    프로젝트 전체 명단을 구조화 하는 작업 구상

---
+++ team-modeling/memo.ts
@@ -0,0 +1,33 @@
+interface TeamProjectInfo {
+  teamName: string;
+  teamProjectManagetName: string;
+  teamMembers: string[];
+}
+
+const KDTtotalTeamInfo: {
+  teamProjectC: TeamProjectInfo[];
+  teamProjectA: TeamProjectInfo[];
+  teamProjectB: TeamProjectInfo[];
+} = {
+  teamProjectC: [
+    {
+      teamName: "",
+      teamProjectManagetName: "",
+      teamMembers: []
+    }
+  ],
+  teamProjectA: [
+    {
+      teamName: "",
+      teamProjectManagetName: "",
+      teamMembers: []
+    }
+  ],
+  teamProjectB: [
+    {
+      teamName: "",
+      teamProjectManagetName: "",
+      teamMembers: []
+    }
+  ],
+};
